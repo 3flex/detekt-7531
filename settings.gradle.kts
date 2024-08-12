f7140c17b3217b5d761c5361219bfdb1b00f19e6
Matthew Haughton <3flex@users.noreply.github.com>
2024-08-12 12:39:02 +1000

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,10 @@
+pluginManagement {
+    repositories {
+        maven(url="https://oss.sonatype.org/content/repositories/snapshots/")
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("com.gradle.develocity") version("3.17.6")
 }
