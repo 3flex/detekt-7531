dc3ce19f8ba34c62cbada417c161f18423cc32ee
Matthew Haughton <3flex@users.noreply.github.com>
2024-08-12 12:38:02 +1000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,10 @@
 import io.gitlab.arturbosch.detekt.Detekt
 import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 
-buildscript {
-    repositories {
-        maven(url="https://oss.sonatype.org/content/repositories/snapshots/")
-    }
-    dependencies {
-        classpath("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:main-SNAPSHOT")
-    }
-}
-
 plugins {
     kotlin("multiplatform") version "2.0.20-RC"
+    id("io.gitlab.arturbosch.detekt") version "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:main-SNAPSHOT"
 }
-plugins.apply("io.gitlab.arturbosch.detekt")
 
 detekt {
     debug = true
