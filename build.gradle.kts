5fdb13522474aa6d8cf87814032895123ec01645
Matthew Haughton <3flex@users.noreply.github.com>
2024-08-12 13:13:31 +1000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,16 @@ import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 
 plugins {
     kotlin("multiplatform") version "2.0.20-RC"
+    kotlin("plugin.serialization") version "2.0.20-RC"
     id("io.gitlab.arturbosch.detekt") version "main-SNAPSHOT"
 }
 
 detekt {
     debug = true
+}
+
+dependencies {
+    commonMainImplementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.1")
 }
 
 repositories {
